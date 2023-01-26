e4a7562f9036ebbf7632a829864bb36924d2d2fc
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-01-26 18:49:16 +0900

    URL 링크

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -56,6 +56,13 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data){
