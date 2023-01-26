466fd3d435f592311d994a16b6d12d0c782e6b84
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-01-26 19:22:21 +0900

    속성 값 설정

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -69,6 +69,11 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data){
